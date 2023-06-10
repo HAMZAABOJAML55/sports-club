59fce319f2e0f16d9fb03176b34179c17c35311f
User Name <Email@example.com>
2023-06-10 21:21:41 +0200

    "Users Api"

--- resources/views/layouts/main-sidebar/admin-main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar/admin-main-sidebar.blade.php
@@ -171,18 +171,7 @@
 
 
         <!-- Users-->
-        <li>
-            <a href="javascript:void(0);" data-toggle="collapse" data-target="#Users-icon">
-                <div class="pull-left"><i class="fas fa-users"></i><span class="right-nav-text">{{trans('main_trans.Users')}}</span></div>
-                <div class="pull-right"><i class="ti-plus"></i></div>
-                <div class="clearfix"></div>
-            </a>
-            <ul id="Users-icon" class="collapse" data-parent="#sidebarnav">
-                <li> <a href="fontawesome-icon.html">font Awesome</a> </li>
-                <li> <a href="themify-icons.html">Themify icons</a> </li>
-                <li> <a href="weather-icon.html">Weather icons</a> </li>
-            </ul>
-        </li>
+
 
     </ul>
 </div>
